898f2c5147801bc33aefbecc37520416aa06caa3
yaxyobekuz <pubgn9642@gmail.com>
2025-08-19 16:39:48 +0500

    Refactor TextDraggable component to integrate TipTap editor and DropzoneNode

--- src/components/questions/TextDraggable.jsx
+++ src/components/questions/TextDraggable.jsx
@@ -1,17 +1,23 @@
-// Helpers
-import { convertToHtml } from "../../lib/helpers";
+// Tip tap
+import StarterKit from "@tiptap/starter-kit";
+import { useEditor, EditorContent } from "@tiptap/react";
+
+// Nodes
+import DropzoneNode from "../../format/nodes/DropzoneNode";
 
 const TextDraggable = ({ text, initialNumber, options }) => {
-  // Content
+  const editor = useEditor({
+    content: text,
+    editable: false,
+    extensions: [StarterKit, DropzoneNode(initialNumber, false)],
+  });
+
   return (
     <div className="flex gap-5 w-full">
-      <pre
-        className="flex-1 overflow-auto max-w-max"
-        dangerouslySetInnerHTML={{ __html: convertToHtml(text, initialNumber) }}
-      />
+      <EditorContent editor={editor} className="text-editor" />
 
-      <div>
-        <b className="inline-block mb-2">{options.title}</b>
+      <div className="min-w-max space-y-2 pr-5">
+        <b className="inline-block">{options.title}</b>
 
         {/* Answer options */}
         <ul className="max-w-max rounded-md space-y-2">
