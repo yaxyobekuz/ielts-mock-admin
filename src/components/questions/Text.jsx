167cea683c9756d17f4c6c1039fe3f8b8cb88ea6
yaxyobekuz <pubgn9642@gmail.com>
2025-08-19 16:39:43 +0500

    Refactor Text component to simplify AnswerInputNode integration

--- src/components/questions/Text.jsx
+++ src/components/questions/Text.jsx
@@ -1,73 +1,11 @@
-import { memo, useCallback, useEffect, useState } from "react";
+import { memo } from "react";
 
 // Tip tap
 import StarterKit from "@tiptap/starter-kit";
-import { Node, mergeAttributes } from "@tiptap/core";
 import { useEditor, EditorContent } from "@tiptap/react";
-import { ReactNodeViewRenderer, NodeViewWrapper } from "@tiptap/react";
 
-const AnswerInputComponent = ({ editor, getPos, initialNumber = 1 }) => {
-  const [inputIndex, setInputIndex] = useState(initialNumber);
-
-  const calculateIndex = useCallback(() => {
-    try {
-      let index = initialNumber;
-      const currentPos = getPos();
-
-      editor.state.doc.descendants((node, pos) => {
-        if (node.type.name === "answer-input" && pos < currentPos) {
-          index++;
-        }
-      });
-
-      setInputIndex(index);
-    } catch (error) {
-      console.warn("Error calculating input index:", error);
-    }
-  }, [editor, getPos, initialNumber]);
-
-  useEffect(() => {
-    editor && calculateIndex();
-  }, [editor, calculateIndex]);
-
-  return (
-    <NodeViewWrapper className="inline-block px-1 py-px">
-      <input
-        type="text"
-        placeholder={inputIndex}
-        className="answer-input pr-5"
-        id={`answer-input-${inputIndex}`}
-      />
-    </NodeViewWrapper>
-  );
-};
-
-const AnswerInputNode = (initialNumber) =>
-  Node.create({
-    inline: true,
-    group: "inline",
-    name: "answer-input",
-
-    parseHTML() {
-      return [{ tag: "input[data-name='answer-input']" }];
-    },
-
-    renderHTML({ HTMLAttributes }) {
-      return [
-        "input",
-        mergeAttributes(HTMLAttributes, {
-          type: "text",
-          "data-name": "answer-input",
-        }),
-      ];
-    },
-
-    addNodeView() {
-      return ReactNodeViewRenderer((props) => (
-        <AnswerInputComponent {...props} initialNumber={initialNumber} />
-      ));
-    },
-  });
+// Nodes
+import AnswerInputNode from "../../format/nodes/AnswerInputNode";
 
 const Text = memo(({ text, initialNumber }) => {
   if (!text) return null;
@@ -75,7 +13,7 @@ const Text = memo(({ text, initialNumber }) => {
   const editor = useEditor({
     content: text,
     editable: false,
-    extensions: [StarterKit, AnswerInputNode(initialNumber)],
+    extensions: [StarterKit, AnswerInputNode(initialNumber, false)],
   });
 
   return <EditorContent editor={editor} className="text-editor" />;
