0ea1b8ef5d22fd49ffabf128afee8bf9e56f8258
yaxyobekuz <pubgn9642@gmail.com>
2025-08-22 01:03:22 +0500

    Add Reading page

---
+++ src/pages/Reading.jsx
@@ -0,0 +1,161 @@
+import { useMemo } from "react";
+import { Link, useParams } from "react-router-dom";
+
+// Components
+import Icon from "../components/Icon";
+
+// Hooks
+import useModule from "../hooks/useModule";
+
+// Icons
+import penIcon from "../assets/icons/pen.svg";
+
+// Data
+import questionsType from "../data/questionsType";
+import usePathSegments from "../hooks/usePathSegments";
+
+const questionsMap = {};
+questionsType.forEach((q) => (questionsMap[q.value] = q.component));
+const TextComponent = questionsMap["text"];
+
+const Reading = () => {
+  const { partNumber, testId } = useParams();
+  const { pathSegments } = usePathSegments();
+  const module = pathSegments[4];
+
+  const { getModuleData } = useModule(module, testId);
+  const parts = getModuleData();
+
+  // Calculate current part and cumulative question count
+  const { currentPart, cumulativeQuestions } = useMemo(() => {
+    const partNum = parseInt(partNumber);
+    const part = parts?.find((p) => p.number === partNum);
+    const cumulative = parts
+      ?.slice(0, partNum - 1)
+      ?.reduce((acc, part) => acc + part.totalQuestions, 0);
+
+    return {
+      currentPart: part,
+      cumulativeQuestions: cumulative,
+    };
+  }, [parts, partNumber, module]);
+
+  const { description, sections, text } = currentPart || {};
+
+  // Return error if part not found
+  if (!currentPart) {
+    return (
+      <div className="container">
+        <div className="py-8">
+          <div className="w-full bg-red-50 py-3 px-4 mb-5 rounded-xl border border-red-300">
+            <p className="text-red-700">Part not found</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <div className="pt-8">
+        {/* Part header */}
+        <div className="w-full bg-gray-50 py-3 px-4 mb-5 rounded-xl border border-gray-300">
+          <h1 className="mb-1 font-bold">Part {partNumber}</h1>
+          <p>{description || "Description not found!"}</p>
+        </div>
+
+        {/* Main */}
+        <div className="grid grid-cols-2 gap-3.5 pb-5">
+          {/* Part text */}
+          <div className="w-full bg-gray-50 p-5 rounded-xl border">
+            {/* Edit button */}
+            <EditButton
+              className="max-w-max ml-auto"
+              to={`/tests/test/${testId}/edit/${module}/${partNumber}/part-text`}
+            />
+
+            <TextComponent text={text} initialNumber={0} />
+          </div>
+
+          {/* Sections content */}
+          <div className="w-full">
+            {sections?.map((section, index) => {
+              const prevSectionsTotalQuestions = sections
+                .slice(0, index)
+                .reduce((acc, sec) => acc + sec.questionsCount, 0);
+
+              return (
+                <Section
+                  index={index}
+                  module={module}
+                  testId={testId}
+                  section={section}
+                  partNumber={partNumber}
+                  key={`${section.questionType}-${index}`}
+                  initialQuestionNumber={
+                    prevSectionsTotalQuestions + cumulativeQuestions + 1
+                  }
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Individual section component
+const Section = ({
+  index,
+  testId,
+  module,
+  section,
+  partNumber,
+  initialQuestionNumber,
+}) => {
+  const { title, description, type } = section;
+  const QuestionComponent = questionsMap[type];
+
+  return (
+    <section
+      id={`s-${index}`}
+      className="mb-6 px-5 py-4 bg-gray-50 rounded-xl border last:mb-0"
+    >
+      {/* Top */}
+      <div className="flex items-start justify-between gap-5">
+        {/* Section details */}
+        <div className="mb-4 space-y-2">
+          <h2 className="font-bold">{title}</h2>
+          <p>{description}</p>
+        </div>
+
+        {/* Edit button */}
+        <EditButton
+          to={`/tests/test/${testId}/edit/${module}/${partNumber}/${type}/${index}`}
+        />
+      </div>
+
+      {/* Main */}
+      {QuestionComponent ? (
+        <QuestionComponent {...section} initialNumber={initialQuestionNumber} />
+      ) : (
+        <div className="bg-gray-50 border rounded p-4 text-yellow-800">
+          Unknown question type: {type}
+        </div>
+      )}
+    </section>
+  );
+};
+
+const EditButton = ({ to, className = "" }) => (
+  <Link
+    to={to}
+    className={`${className} flex items-center justify-center gap-3.5 h-9 px-5 bg-blue-500 rounded-md text-white`}
+  >
+    <span>Tahrirlash</span>
+    <Icon size={20} src={penIcon} alt="Pen" className="size-5" />
+  </Link>
+);
+
+export default Reading;
