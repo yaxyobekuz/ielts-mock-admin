b48b77389f9518c580a2fd2f7baa3dc2dec7ce79
yaxyobekuz <pubgn9642@gmail.com>
2025-09-05 11:29:27 +0500

    Add partsApi with CRUD methods

---
+++ src/api/parts.api.js
@@ -0,0 +1,9 @@
+import api from "./api";
+
+export const partsApi = {
+  getAll: async () => await api.get("/api/parts"),
+  getById: async (id) => await api.get(`/api/parts/${id}`),
+  create: async (data) => await api.post("/api/parts", data),
+  remove: async (id) => await api.delete(`/api/parts/${id}`),
+  update: async (id, data) => await api.put(`/api/parts/${id}`, data),
+};
