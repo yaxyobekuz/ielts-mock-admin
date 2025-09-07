279f42e061a95b476cceae126e87c436317b04ab
yaxyobekuz <pubgn9642@gmail.com>
2025-09-07 15:48:51 +0500

    Add optional config support to uploadApi methods

--- src/api/upload.api.js
+++ src/api/upload.api.js
@@ -1,19 +1,21 @@
 import api from "./api";
 
 export const uploadApi = {
-  uploadPhoto: async (file) => {
+  uploadPhoto: async (file, config = {}) => {
     const formData = new FormData();
     formData.append("photo", file);
     return await api.post("/api/upload/photo", formData, {
       headers: { "Content-Type": "multipart/form-data" },
+      ...config,
     });
   },
 
-  uploadPhotos: async (files) => {
+  uploadPhotos: async (files, config = {}) => {
     const formData = new FormData();
     files.forEach((file) => formData.append("photos", file));
     return await api.post("/api/upload/photos", formData, {
       headers: { "Content-Type": "multipart/form-data" },
+      ...config,
     });
   },
 };
