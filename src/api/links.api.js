d015fbf2f0570bd3737462e4b2efb71048760806
yaxyobekuz <pubgn9642@gmail.com>
2025-09-11 21:37:14 +0500

    Update linksApi get method to use query parameter for testId

--- src/api/links.api.js
+++ src/api/links.api.js
@@ -4,6 +4,6 @@ export const linksApi = {
   getById: async (id) => await api.get(`/api/links/${id}`),
   create: async (data) => await api.post("/api/links", data),
   preview: async (id) => await api.get(`/api/links/${id}/preview`),
-  get: async (testId) => await api.get("/api/links?mine=true", { testId }),
   addUsage: async (id, data) => await api.post(`/api/links/${id}/usage`, data),
+  get: async (testId) => await api.get(`/api/links?mine=true&testId=${testId}`),
 };
