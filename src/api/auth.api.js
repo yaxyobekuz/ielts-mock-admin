bc584259f28d01239f18072f1c6ee036ecab0116
yaxyobekuz <pubgn9642@gmail.com>
2025-09-18 21:06:29 +0500

    Add role param to register auth api

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -4,5 +4,5 @@ export const authApi = {
   profile: async () => await api.get("/api/auth/profile"),
   login: async (data) => await api.post("/api/auth/login", data),
   verify: async (data) => await api.post("/api/auth/verify", data),
-  register: async (data) => await api.post("/api/auth/register", data),
+  register: async (data) => await api.post("/api/auth/register?role=supervisor", data),
 };
