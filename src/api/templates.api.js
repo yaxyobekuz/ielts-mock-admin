b6a4c7dfbf7576642b7c2186c0acd0c6bf9ff93a
yaxyobekuz <pubgn9642@gmail.com>
2025-10-01 20:29:02 +0500

    Add random query parameter to templatesApi getById function

--- src/api/templates.api.js
+++ src/api/templates.api.js
@@ -2,5 +2,5 @@ import api from "./api";
 
 export const templatesApi = {
   get: async () => await api.get("/api/templates"),
-  getById: async (id) => await api.get(`/api/templates/${id}`),
+  getById: async (id) => await api.get(`/api/templates/${id}?random=true`),
 };
