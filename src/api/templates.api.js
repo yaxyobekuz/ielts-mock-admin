62c7da4e108f061cedab1c87436031ef7ec7dd62
yaxyobekuz <pubgn9642@gmail.com>
2025-10-01 23:19:57 +0500

    Add use method to templatesApi for using a template

--- src/api/templates.api.js
+++ src/api/templates.api.js
@@ -3,4 +3,5 @@ import api from "./api";
 export const templatesApi = {
   get: async () => await api.get("/api/templates"),
   getById: async (id) => await api.get(`/api/templates/${id}?random=true`),
+  use: async (id, data) => await api.post(`/api/templates/${id}/use`, data),
 };
