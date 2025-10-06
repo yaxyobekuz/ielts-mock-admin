dcc5a335cf69487048044024e5260ffb2e87e89d
yaxyobekuz <pubgn9642@gmail.com>
2025-10-06 14:01:23 +0500

    Add data prop to LineChart component

--- src/components/charts/LineChart.jsx
+++ src/components/charts/LineChart.jsx
@@ -1,6 +1,6 @@
 import { ResponsiveLine } from "@nivo/line";
 
-const data = [
+const defaultData = [
   {
     id: "Umumiy",
     data: [
@@ -14,39 +14,41 @@ const data = [
   },
 ];
 
-const LineChart = ({ className = "" }) => (
-  <div className={className}>
-    <ResponsiveLine
-      data={data}
-      pointSize={6}
-      useMesh={true}
-      lineWidth={2.5}
-      curve="cardinal"
-      enableGridX={false}
-      pointBorderWidth={2}
-      xScale={{ type: "point" }}
-      pointColor={{ theme: "background" }}
-      pointBorderColor={{ from: "serie.color" }}
-      margin={{ top: 12, right: 32, bottom: 32, left: 52 }}
-      axisLeft={{ legend: "Test yechishlar", legendOffset: -36 }}
-      yScale={{ type: "linear", min: "auto", max: "auto", stacked: false }}
-      // Gradient color
-      enableArea={true}
-      areaOpacity={0.5}
-      colors={["#3b82f6"]}
-      fill={[{ match: "*", id: "gradientA" }]}
-      defs={[
-        {
-          id: "gradientA",
-          type: "linearGradient",
-          colors: [
-            { offset: 0, color: "#3b82f6", opacity: 0.4 },
-            { offset: 100, color: "#3b82f6", opacity: 0 },
-          ],
-        },
-      ]}
-    />
-  </div>
-);
+const LineChart = ({ className = "", data = defaultData }) => {
+  return (
+    <div className={className}>
+      <ResponsiveLine
+        data={data}
+        pointSize={6}
+        useMesh={true}
+        lineWidth={2.5}
+        curve="cardinal"
+        enableGridX={false}
+        pointBorderWidth={2}
+        xScale={{ type: "point" }}
+        pointColor={{ theme: "background" }}
+        pointBorderColor={{ from: "serie.color" }}
+        margin={{ top: 12, right: 32, bottom: 32, left: 52 }}
+        axisLeft={{ legend: "Test yechishlar", legendOffset: -36 }}
+        yScale={{ type: "linear", min: "auto", max: "auto", stacked: false }}
+        // Gradient color
+        enableArea={true}
+        areaOpacity={0.5}
+        colors={["#3b82f6"]}
+        fill={[{ match: "*", id: "gradientA" }]}
+        defs={[
+          {
+            id: "gradientA",
+            type: "linearGradient",
+            colors: [
+              { offset: 0, color: "#3b82f6", opacity: 0.4 },
+              { offset: 100, color: "#3b82f6", opacity: 0 },
+            ],
+          },
+        ]}
+      />
+    </div>
+  );
+};
 
 export default LineChart;
