4b7ca40a5d8de302632542c1a3faee4dd72fb0c7
yaxyobekuz <pubgn9642@gmail.com>
2025-10-13 21:00:15 +0500

    Add allowedImageTypes module for image format validation

---
+++ src/data/allowedImageTypes.js
@@ -0,0 +1,17 @@
+const allowedImageTypes = [
+  "image/jpg",
+  "image/jpeg",
+  "image/png",
+  "image/gif",
+  "image/bmp",
+  "image/tiff",
+  "image/tif",
+  "image/webp",
+  "image/heic",
+  "image/heif",
+  "image/avif",
+  "image/ico",
+  "image/jp2",
+];
+
+export default allowedImageTypes;
