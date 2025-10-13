1aa5f20d0f906d7620c5088736ea93212138c0af
yaxyobekuz <pubgn9642@gmail.com>
2025-10-13 20:59:03 +0500

    Refactor Button component to ensure consistent variant and size class definitions

--- src/components/form/Button.jsx
+++ src/components/form/Button.jsx
@@ -8,12 +8,13 @@ const Button = ({
 }) => {
   const variants = {
     neutral: "bg-gray-100 hover:bg-gray-200 disabled:!bg-gray-100",
-    primary: "bg-blue-500 text-white hover:bg-blue-600 disabled:!bg-blue-500",
     danger: "bg-red-500 text-white hover:bg-red-600 disabled:!bg-red-500",
-    lightBlue: "bg-blue-100 text-blue-500 hover:bg-blue-200 disabled:!bg-blue-100",
+    primary: "bg-blue-500 text-white hover:bg-blue-600 disabled:!bg-blue-500",
+    lightBlue: `bg-blue-100 text-blue-500 hover:bg-blue-200 disabled:!bg-blue-100`,
   };
 
   const sizeClasses = {
+    none: "",
     sm: "h-8 px-4 rounded-md",
     md: "h-9 px-5 rounded-lg",
     lg: "h-10 px-5 rounded-lg",
