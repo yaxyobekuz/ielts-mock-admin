b0fed7f136d115ee8b79ac7eb86be8fa608e0964
yaxyobekuz <pubgn9642@gmail.com>
2025-10-13 12:40:10 +0500

    Rename remove method to delete in partsApi for consistency

--- src/api/parts.api.js
+++ src/api/parts.api.js
@@ -4,6 +4,6 @@ export const partsApi = {
   getAll: async () => await api.get("/api/parts"),
   getById: async (id) => await api.get(`/api/parts/${id}`),
   create: async (data) => await api.post("/api/parts", data),
-  remove: async (id) => await api.delete(`/api/parts/${id}`),
+  delete: async (id) => await api.delete(`/api/parts/${id}`),
   update: async (id, data) => await api.put(`/api/parts/${id}`, data),
 };
