a53dcee159939581f96cedbae44f37f4c9a3cd76
yaxyobekuz <pubgn9642@gmail.com>
2025-10-15 12:08:19 +0500

    Add updatePermissions method to teachersApi for managing teacher permissions

--- src/api/teachers.api.js
+++ src/api/teachers.api.js
@@ -5,4 +5,7 @@ export const teachersApi = {
   getById: async (id) => await api.get(`/api/teachers/${id}`),
   create: async (data) => await api.post("/api/teachers", data),
   update: async (id, data) => await api.put(`/api/teachers/${id}`, data),
+  updatePermissions: async (id, data) => {
+    return await api.put(`/api/teachers/${id}/permissions`, data);
+  },
 };
