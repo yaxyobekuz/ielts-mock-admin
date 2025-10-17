7f04d5628b1c5a829376efc7b8d449946a57535a
yaxyobekuz <pubgn9642@gmail.com>
2025-10-17 19:14:46 +0500

    Refactor CreateTeacherModal to streamline structure and enhance loading state management

--- src/components/modal/CreateTeacherModal.jsx
+++ src/components/modal/CreateTeacherModal.jsx
@@ -1,18 +1,3 @@
-// Ui components
-import {
-  Dialog,
-  DialogTitle,
-  DialogHeader,
-  DialogContent,
-  DialogDescription,
-} from "@/components/ui/dialog";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerFooter,
-  DrawerContent,
-} from "@/components/ui/drawer";
-
 // Components
 import Input from "../form/Input";
 import Button from "../form/Button";
@@ -29,63 +14,24 @@ import { useNavigate } from "react-router-dom";
 // Api
 import { teachersApi } from "@/api/teachers.api";
 
+// Components
+import ResponsiveModal from "../ResponsiveModal";
+
 // Hooks
-import useModal from "@/hooks/useModal";
 import useStore from "@/hooks/useStore";
-import useMediaQuery from "@/hooks/useMediaQuery";
 import useObjectState from "@/hooks/useObjectState";
 
-const CreateTeacherModal = () => {
-  const isDesktop = useMediaQuery("(min-width: 768px)");
-  const { closeModal, isOpen } = useModal("createTeacher");
+const CreateTeacherModal = () => (
+  <ResponsiveModal
+    name="createTeacher"
+    title="Ustoz qo'shish"
+    description="Yangi ustoz qo'shish uchun telefon raqam va parolni kiriting"
+  >
+    <Content />
+  </ResponsiveModal>
+);
 
-  const content = {
-    title: "Ustoz qo'shish",
-    body: <Body close={closeModal} />,
-    description: `Yangi ustoz qo'shish uchun telefon raqam va parolni kiriting`,
-  };
-
-  if (isDesktop) {
-    return (
-      <Dialog open={isOpen} onOpenChange={closeModal}>
-        <DialogContent className="sm:max-w-[425px]">
-          {/* Header */}
-          <DialogHeader>
-            <DialogTitle>{content.title}</DialogTitle>
-            <DialogDescription>{content.description}</DialogDescription>
-          </DialogHeader>
-
-          {/* Body */}
-          {content.body}
-        </DialogContent>
-      </Dialog>
-    );
-  }
-
-  return (
-    <Drawer open={isOpen} onOpenChange={closeModal}>
-      <DrawerContent>
-        {/* Header */}
-        <DialogHeader>
-          <DialogTitle>{content.title}</DialogTitle>
-          <DialogDescription>{content.description}</DialogDescription>
-        </DialogHeader>
-
-        {/* Body */}
-        {content.body}
-
-        {/* Footer */}
-        <DrawerFooter className="pt-2">
-          <DrawerClose asChild>
-            <button variant="outline">Bekor qilish</button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  );
-};
-
-const Body = ({ close }) => {
+const Content = ({ close, isLoading, setIsLoading }) => {
   const navigate = useNavigate();
   const { getData, updateProperty } = useStore("teachers");
   const { isLoading: isTeachersLoading, data: teachers } = getData();
@@ -99,34 +45,38 @@ const Body = ({ close }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (isLoading) return;
 
-    if (!password || password.trim().length < 4) {
+    const formattedPassword = password?.trim() || "";
+    const formattedPhone = extractNumbers(phone) || "";
+
+    if (formattedPassword.length < 4) {
       return toast.error("Parol juda ham qisqa");
     }
 
-    if (!phone || extractNumbers(phone).trim().length !== 12) {
+    if (formattedPhone.length !== 12) {
       return toast.error("Telefon raqam noto'g'ri kiritildi");
     }
 
-    toast.promise(
-      teachersApi
-        .create({ password: password.trim(), phone: extractNumbers(phone) })
-        .then(({ teacher, code }) => {
-          if (code !== "teacherCreated") throw new Error();
+    setIsLoading(true);
+    let success = false;
 
-          navigate(`/teachers/${teacher._id}`);
+    teachersApi
+      .create({ password: formattedPassword, phone: formattedPhone })
+      .then(({ teacher, code }) => {
+        if (code !== "teacherCreated") throw new Error();
 
-          if (!isTeachersLoading) {
-            updateProperty("data", [teacher, ...teachers]);
-          }
-        })
-        .finally(close),
-      {
-        error: "Ustoz qo'shilmadi!",
-        success: "Ustoz qo'shildi!",
-        loading: "Ustoz qo'shilmoqda...",
-      }
-    );
+        success = true;
+        navigate(`/teachers/${teacher._id}`);
+
+        if (!isTeachersLoading) {
+          updateProperty("data", [teacher, ...teachers]);
+        }
+      })
+      .finally(() => {
+        success && close();
+        setIsLoading(false);
+      });
   };
 
   return (
@@ -165,14 +115,14 @@ const Body = ({ close }) => {
       <div className="flex justify-end gap-5 w-full">
         <Button
           type="button"
-          onClick={()=> close()}
           className="w-32"
           variant="neutral"
+          onClick={() => close()}
         >
           Bekor qilish
         </Button>
 
-        <Button type="submit" className="w-32">
+        <Button disabled={isLoading} className="w-32">
           Qo'shish
         </Button>
       </div>
