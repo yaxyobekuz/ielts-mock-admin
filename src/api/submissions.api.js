9702b0c09a88c8f190b4bd6e3c1c9e917c65eb2b
yaxyobekuz <pubgn9642@gmail.com>
2025-10-19 11:54:05 +0500

    Refactor submissionsApi to accept parameters in get method for improved API flexibility.

--- src/api/submissions.api.js
+++ src/api/submissions.api.js
@@ -1,7 +1,7 @@
 import api from "./api";
 
 export const submissionsApi = {
-  get: async () => await api.get("/api/submissions"),
   getById: async (id) => await api.get(`/api/submissions/${id}`),
+  get: async (params) => await api.get("/api/submissions", { params }),
   update: async (id, data) => await api.put(`/api/submissions/${id}`, data),
 };
