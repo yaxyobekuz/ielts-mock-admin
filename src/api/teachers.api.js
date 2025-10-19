a1c1874c15660d0202b52c87d938dae1cee201c7
yaxyobekuz <pubgn9642@gmail.com>
2025-10-19 13:09:50 +0500

    Refactor API methods in resultsApi, teachersApi, and templatesApi to accept parameters in the get method for improved API flexibility.

--- src/api/teachers.api.js
+++ src/api/teachers.api.js
@@ -1,9 +1,9 @@
 import api from "./api";
 
 export const teachersApi = {
-  get: async () => await api.get("/api/teachers"),
   getById: async (id) => await api.get(`/api/teachers/${id}`),
   create: async (data) => await api.post("/api/teachers", data),
+  get: async (params = {}) => await api.get("/api/teachers", { params }),
   update: async (id, data) => await api.put(`/api/teachers/${id}`, data),
   updatePermissions: async (id, data) => {
     return await api.put(`/api/teachers/${id}/permissions`, data);
--- src/api/templates.api.js
+++ src/api/templates.api.js
@@ -1,7 +1,7 @@
 import api from "./api";
 
 export const templatesApi = {
-  get: async () => await api.get("/api/templates"),
+  get: async (params = {}) => await api.get("/api/templates", { params }),
   getById: async (id) => await api.get(`/api/templates/${id}?random=true`),
   use: async (id, data) => await api.post(`/api/templates/${id}/use`, data),
   create: async (data, config = {}) => {
