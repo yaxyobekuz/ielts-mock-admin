739440ff9ab5a88c2ee6ea8b1412952efb1e6940
yaxyobekuz <pubgn9642@gmail.com>
2025-10-30 10:15:52 +0500

    Add getNames function to teachersApi for fetching teacher names list

--- src/api/teachers.api.js
+++ src/api/teachers.api.js
@@ -4,6 +4,7 @@ export const teachersApi = {
   getById: async (id) => await api.get(`/api/teachers/${id}`),
   create: async (data) => await api.post("/api/teachers", data),
   delete: async (id) => await api.delete(`/api/teachers/${id}`),
+  getNames: async () => await api.get("/api/teachers/names/list"),
   get: async (params = {}) => await api.get("/api/teachers", { params }),
   update: async (id, data) => await api.put(`/api/teachers/${id}`, data),
   updatePermissions: async (id, data) => {
