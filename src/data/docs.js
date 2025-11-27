016ac3f8bcb61e46e5d1ef2d3a467a7ab0127930
yaxyobekuz <pubgn9642@gmail.com>
2025-11-27 16:16:25 +0500

    Add docs data

---
+++ src/data/docs.js
@@ -0,0 +1,11 @@
+import kirish from "@/assets/videos/cdielts-kirish.mp4";
+
+const docs = [
+  {
+    videoUrl: kirish,
+    title: `Kuzatuvchi va ustoz uchun kirish qo'llanma`,
+    description: `Videoda kuzatuvchi va ustoz rolidagi foydalanuvchilar uchun kirish funksiyalar va imkoniyatlar haqida ma'lumot berilgan.`,
+  },
+];
+
+export default docs;
